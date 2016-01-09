cba3b496cff21057e1eb32fc76034e921f98dc89
Administrator <kamiakuma2013@gmail.com>
2016-01-09 14:59:59 +0700

    ใส่เสียงตอนกดปุ่ม

--- app/src/main/java/rru/kao/taweevat/mytraffic/MainActivity.java
+++ app/src/main/java/rru/kao/taweevat/mytraffic/MainActivity.java
@@ -1,13 +1,52 @@
 package rru.kao.taweevat.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ZoomButtonsController;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Buttin Controller
+        ButtonsController();
+
+    } // Main Method
+
+    private void ButtonsController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effecf
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),
+                        R.raw.bee);
+                buttonMediaPlayer.start();
+            }
+        });
+
+    } // buttonController
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+
     }
-}
+
+} // Main Class นี่คือคลาสหลัก
